cf56c6257e0351fab3fe27b060b60a08b61c8329
jimmy2 <jimmy2289@gmail.com>
2023-07-16 19:31:48 +1000

    added WebSite schema. favicons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://triffoona.com/",
   trailingSlash: "never",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), astroImageTools],
 });
