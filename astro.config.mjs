cea1beb2f3d0b28351bfc57ba0886fb3e8c35f2b
jimmy2 <jimmy2289@gmail.com>
2023-07-17 21:35:07 +1000

    added mdx. added privacy + terms. midjourney img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 import { astroImageTools } from "astro-imagetools";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://triffoona.com/",
   trailingSlash: "never",
-  integrations: [tailwind(), sitemap(), astroImageTools],
+  integrations: [tailwind(), sitemap(), astroImageTools, mdx()]
 });
