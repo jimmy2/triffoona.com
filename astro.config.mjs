cd60fbd6a9146cca1eec484defaa84c93e990cf5
jimmy2 <jimmy2289@gmail.com>
2023-07-19 21:08:53 +1000

    replace images with astro images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
-
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://triffoona.com/",
   trailingSlash: "never",
-  integrations: [tailwind(), sitemap(), astroImageTools, mdx()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
